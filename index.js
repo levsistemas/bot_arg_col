5643109056ea8f2745a799fff84d7a6f99a72227
Leandro <132308367+levsistemas@users.noreply.github.com>
2024-02-23 20:00:37 -0300

    Updates index.js
    Agregado archivo keep_alive.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // requerimientos
 const Discord = require('discord.js');
 
+//agregar keep_alive
+const keep_alive = require ('./keep_alive.js');
+
 //definir cliente
 const Client = new Discord.Client({
     intents: 33027
